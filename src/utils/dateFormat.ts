8fdd2a18521fb655b735b211094791eaee7b1b36
JotaDiasDevs <joaovitor.semente.d@gmail.com>
2025-11-09 13:13:44 -0300

    fix(build): remove unused React import and unused 'mes' to resolve TS6133

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -2,7 +2,6 @@
 export function formatarData(data: string): string {
   const date = new Date(data + 'T00:00:00')
   const dia = date.getDate().toString().padStart(2, '0')
-  const mes = (date.getMonth() + 1).toString().padStart(2, '0')
   const ano = date.getFullYear()
   
   const diasSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
